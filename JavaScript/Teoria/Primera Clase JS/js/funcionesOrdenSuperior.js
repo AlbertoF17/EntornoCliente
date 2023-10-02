c21722ecd7c0c67df0aee9c5fa540c5451aa93ed
AlbertoF17 <albertofergom@gmail.com>
2023-10-02 16:54:42 +0200

    EntornoCliente

--- JavaScript/Teoria/Primera Clase JS/js/funcionesOrdenSuperior.js
+++ JavaScript/Teoria/Primera Clase JS/js/funcionesOrdenSuperior.js
@@ -1,3 +1,4 @@
+//EJERCICIO 1
 console.log("1) Define una función que reciba un número n y devuelva si es primo o no");
 function esPrimo(numero) {
     if (numero <= 1) {
@@ -11,6 +12,7 @@ function esPrimo(numero) {
     return true;
 }
 
+//EJERCICIO 2
 console.log("2) Dado el array [5,1,7,8,2,4,6,3,9], comprueba si hay algún número primo");
 let array1 = [5, 1, 7, 8, 2, 4, 6, 3, 9];
 let cont = 0;
@@ -19,10 +21,11 @@ array1.forEach(element => {
         cont++;
     }
 });
-console.log("Hay " + cont + " números primos");
+console.log(`Hay ${cont} números primos`);
 
+//EJERCICIO 3
 console.log("3) Dado el array [5,1,7,8,2,4,6,3,9], comprueba si todos son números primos");
-cont = array1.length - 1;
+cont = array1.length;
 array1.forEach(element => {
     if (esPrimo(element)) {
         cont--;
@@ -34,6 +37,7 @@ if (cont === 0) {
     console.log("No todos los números son primos");
 }
 
+//EJERCICIO 4
 console.log("4) Dado el array [5,1,7,8,2,4,6,3,9], comprueba en que posición está el primer número primo");
 cont = -1;
 for (let i = 0; i < array1.length; i++) {
@@ -44,11 +48,12 @@ for (let i = 0; i < array1.length; i++) {
 }
 
 if (cont !== -1) {
-    console.log("El primer número primo está en la posición: " + cont);
+    console.log(`El primer número primo está en la posición: ${cont}`);
 } else {
     console.log("No se encontraron números primos en el array.");
 }
 
+//EJERCICIO 5
 console.log("5) Dado el array [5,1,7,8,2,4,6,3,9], comprueba cual es el primer número primo");
 cont = -1;
 for (let i = 0; i < array1.length; i++) {
@@ -58,11 +63,12 @@ for (let i = 0; i < array1.length; i++) {
     }
 }
 if (cont !== -1) {
-    console.log("El primer número primo es el: " + cont);
+    console.log(`El primer número primo es el: ${cont}`);
 } else {
     console.log("No se encontraron números primos en el array.");
 }
 
+//EJERCICIO 6
 console.log("6) Dado el array [5,1,7,8,2,4,6,3,9], quédate sólo con los números primos");
 cont = 0;
 let arrayPrimos = [];
@@ -73,15 +79,17 @@ array1.forEach(element => {
     }
 });
 
-console.log("El array de sólo los números primos es: " + arrayPrimos);
+console.log(`El array de sólo los números primos es: ${arrayPrimos}`);
 
+//EJERCICIO 7
 console.log("7)  Dado el array de números primos, dame el producto de todos los elementos del array");
 let producto = 1;
 arrayPrimos.forEach(element => {
     producto *= element;
 })
-console.log("El producto de los elementos del array de números primos es: " + producto);
+console.log(`El producto de los elementos del array de números primos es: ${producto}`);
 
+//EJERCICIO 8
 console.log("8)  Define una función que reciba un String y devuelva si es palíndromo o no");
 function esPalindromo(string) {
     string = string.toLowerCase().replace(/ /g, '');
@@ -89,6 +97,7 @@ function esPalindromo(string) {
     return string === stringInvertido;
 }
 
+//EJERCICIO 9
 console.log('9) Dado el array ["ana", "mesa", "radar","seres","folio","sudoku","amor a roma", "sol", "mar", "oso", "salas"], comprueba si hay algún palíndromo');
 let array2 = ["ana", "mesa", "radar", "seres", "folio", "sudoku", "amor a roma", "sol", "mar", "oso", "salas"];
 cont = 0;
@@ -101,11 +110,12 @@ array2.forEach(element => {
 if (cont === 0) {
     console.log("No hay ningún palíndromo");
 } else {
-    console.log("Hay " + cont + " palíndromos");
+    console.log(`Hay ${cont} palíndromos`);
 }
 
+//EJERCICIO 10
 console.log('10) Dado el array ["ana", "mesa", "radar","seres","folio","sudoku","amor a roma", "sol", "mar", "oso", "salas"], comprueba si todos son palíndromos');
-cont = array2.length - 1;
+cont = array2.length;
 array2.forEach(element => {
     if (esPalindromo(element)) {
         cont--;
@@ -117,6 +127,7 @@ if (cont === 0) {
     console.log("No todos los strings del array son palíndromos");
 }
 
+//EJERCICIO 11
 console.log('11) Dado el array ["ana", "mesa", "radar","seres","folio","sudoku","amor a roma", "sol", "mar", "oso", "salas"], comprueba en que posición está el primer palíndromo');
 cont = -1;
 for (let i = 0; i < array2.length; i++) {
@@ -127,11 +138,12 @@ for (let i = 0; i < array2.length; i++) {
 }
 
 if (cont !== -1) {
-    console.log("El primer palíndromo está en la posición: " + cont);
+    console.log(`El primer palíndromo está en la posición: ${cont}`);
 } else {
     console.log("No se encontraron palíndromos en el array.");
 }
 
+//EJERCICIO 12
 console.log('12) Dado el array ["ana", "mesa", "radar","seres","folio","sudoku","amor a roma", "sol", "mar", "oso", "salas"], comprueba cual es el primer palíndromo');
 cont = -1;
 for (let i = 0; i < array2.length; i++) {
@@ -142,11 +154,12 @@ for (let i = 0; i < array2.length; i++) {
 }
 
 if (cont !== -1) {
-    console.log("El primer palíndromo es la cadena: " + cont);
+    console.log(`El primer palíndromo es la cadena: ${cont}`);
 } else {
     console.log("No se encontraron palíndromos en el array.");
 }
 
+//EJERCICIO 13
 console.log('13) Dado el array ["ana", "mesa", "radar","seres","folio","sudoku","amor a roma", "sol", "mar", "oso", "salas"], quédate sólo con los palíndromos');
 let arrayPalindromos = [];
 array2.forEach(element => {
@@ -154,16 +167,18 @@ array2.forEach(element => {
         arrayPalindromos.push(element);
     }
 });
-console.log("El array de palíndromos es: " + arrayPalindromos);
+console.log(`El array de palíndromos es: ${arrayPalindromos}`);
 
+//EJERCICIO 14
 console.log("14)  Dado el array de números palíndromos, dame la concatenación de todos los elementos del array");
 let concatenacion = "";
 arrayPalindromos.forEach(element => {
     concatenacion += element;
 })
-console.log("La concatenación del array de palíndromos es: " + concatenacion);
+console.log(`La concatenación del array de palíndromos es: ${concatenacion}`);
 
-//Otros:
+//OTROS:
+//EJERCICIO 1
 console.log("1) Dado un array de números, verifica si hay algún número negativo");
 cont = 0;
 arrayNeg = [3, 45, 3, -2, 1, 5, 3];
@@ -173,13 +188,14 @@ arrayNeg.forEach(element => {
     }
 });
 if (cont > 1) {
-    console.log("En el array existen " + cont + " números negativos")
+    console.log(`En el array existen ${cont} números negativos`);
 } else if (cont > 0) {
-    console.log("En el array existen " + cont + " número negativo")
+    console.log(`En el array existen " + ${cont} + " número negativo`);
 } else {
-    console.log("En el array no existen números negativos")
+    console.log("En el array no existen números negativos");
 }
 
+//EJERCICIO 2
 console.log("2) Dado un array de Strings, encuentra el índice del primer array que empiece por la letra 'A'");
 cont = -1;
 arrayStrings = ["Pablito", "Juaky", "Alberto", "Jouse", "María"];
@@ -200,8 +216,16 @@ if (cont !== -1) {
     console.log("No se encontró ningún elemento que empiece con 'A' en el array.");
 }
 
+//EJERCICIO 3
 console.log("3) Dado un array de números de 3 o más cifras, calcula la suma de las últimas cifras de cada número");
-let array3Cifras = [214, 300, 435, 824, 476, 535];
+let array3Cifras = [214, 35500, 435, 8724, 476, 7535];
+let suma = 0;
+array3Cifras.forEach(element => {
+    suma += element % 10;
+});
+console.log(`La suma de las últimas cifras de cada número del array es: ${suma}`);
+
+//EJERCICIO 4
 console.log("4) Dado un array de arrays, filtra el array para que sólo queden los arrays de dimensión 5");
 function filtrar5D(array, startD = 0) {
     return array.filter(e => {
@@ -217,6 +241,7 @@ const array6D = [[5], [1, 0, [2, 0, 4, [3, [4, [5, 6]]]]], [3, [0, 5]], [[3, [0,
 const array5D = filtrar5D(array6D);
 console.log(array5D);
 
+//EJERCICIO 5
 console.log("5) Dado un array de Strings, encuentra el primer array de longitud 3");
 cont = -1;
 arrayStr = ["Pablito", "Juaky", "Alberto", "Ana", "María"];
@@ -237,5 +262,18 @@ if (cont !== -1) {
     console.log("No se encontró ningún elemento que empiece con 'A' en el array.");
 }
 
+//EJERCICIO 6
 console.log("6) Dado un array de números, verifica que todos los números sean de 2 cifras o más");
+let arrayNums2Cifras = [22, 443, 21, 23, 53];
+cont = arrayNums2Cifras.length;
+for (let i = 0; i < arrayNums2Cifras.length; i++) {
+    if (arrayNums2Cifras[i] > 9) {
+        cont--;
+    }
+}
 
+if (cont === 0) {
+    console.log("Todos los números son de al menos 2 cifras.");
+} else {
+    console.log("Al menos uno de los números no tiene 2 cifras o más.");
+}
