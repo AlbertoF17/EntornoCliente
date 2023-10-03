a3ed76f384a7d5853d75402315cac02c92b59324
AlbertoF17 <albertofergom@gmail.com>
2023-10-03 20:41:57 +0200

    EntornoCliente

--- JavaScript/Ejercicios y Proyectos/EjercicioEventos/js/app.js
+++ JavaScript/Ejercicios y Proyectos/EjercicioEventos/js/app.js
@@ -71,19 +71,22 @@ const Pablo = {
 let arrayAlumnos = [Alberto, Alicia, Antonio, Carlos, Curro, Jouse, Juanbe, Marcelo, Maria, Miguel, Natalia, Noelia, Pablo];
 let arrayNumeros = [];
 boton.addEventListener("click", () => {
-    let num = 0;
-    do {
-        num = Math.floor(Math.random() * arrayAlumnos.length);
-    } while (arrayNumeros.includes(num));
-    arrayNumeros.push(num);
-    let alumnoSeleccionado = arrayAlumnos[num];
-    let cartaAlumno = document.createElement("div");
-    cartaAlumno.className = "card";
-    let nombre = document.createElement("h1");
-    let apellido = document.createElement("h2");
-    nombre.textContent = alumnoSeleccionado.nombre;
-    apellido.textContent = alumnoSeleccionado.apellido;
-    cartaAlumno.appendChild(nombre);
-    cartaAlumno.appendChild(apellido);
-    cardContainer.appendChild(cartaAlumno);
+    if(arrayNumeros.length<arrayAlumnos.length){
+        let num = 0;
+        do {
+            num = Math.floor(Math.random() * arrayAlumnos.length);
+        } while (arrayNumeros.includes(num));
+        arrayNumeros.push(num);
+        let alumnoSeleccionado = arrayAlumnos[num];
+        let cartaAlumno = document.createElement("div");
+        cartaAlumno.className = "card";
+        let nombre = document.createElement("h1");
+        let apellido = document.createElement("h2");
+        nombre.textContent = alumnoSeleccionado.nombre;
+        apellido.textContent = alumnoSeleccionado.apellido;
+        cartaAlumno.appendChild(nombre);
+        cartaAlumno.appendChild(apellido);
+        cardContainer.appendChild(cartaAlumno);
+    }
+    
 });
