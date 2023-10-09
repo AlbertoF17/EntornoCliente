fb210782b5e21d95c2eac636cb444fe83e464eed
AlbertoF17 <albertofergom@gmail.com>
2023-10-10 00:39:44 +0200

    EntornoCliente

---
+++ JavaScript/Ejercicios y Proyectos/EjercicioHerenciasMetodos/modulos/Vehiculo.js
@@ -0,0 +1,9 @@
+export class Vehiculo {
+    constructor(marca, modelo, matricula, titular) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.titular = titular;
+    }
+
+}
