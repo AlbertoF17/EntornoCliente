b3d39ec83bc30631a56c047af4a8cf6c0f69228e
AlbertoF17 <albertofergom@gmail.com>
2023-10-11 00:25:37 +0200

    EntornoCliente

--- JavaScript/Ejercicios y Proyectos/EjercicioHerenciasMetodos/modulos/Coche.js
+++ JavaScript/Ejercicios y Proyectos/EjercicioHerenciasMetodos/modulos/Coche.js
@@ -1,14 +1,10 @@
-export class Alumno extends Persona {
-    constructor(nombre, apellido, dni, notaProg, notaSist, notaMarc, notaBbdd, notaEntornos, notaFol) {
-        super(nombre, apellido, dni);
-        this.notaProg = notaProg;
-        this.notaSist = notaSist;
-        this.notaMarc = notaMarc;
-        this.notaBbdd = notaBbdd;
-        this.notaEntornos = notaEntornos;
-        this.notaFol = notaFol;
-      }
-    saludar() {
-        return "Hola soy " + this.nombre;
+export class Coche extends Vehículos {
+    constructor(marca, modelo, matricula, titular, tipo) {
+        super(marca, modelo, matricula, titular);
+        this.tipo = tipo;
+    }
+    
+    arrancar() {
+        return "coche arrancado";
     }
 }
