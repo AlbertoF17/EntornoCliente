0ac09be893b523bc3e3f7adf030f496b54546f1d
AlbertoF17 <albertofergom17@gmail.com>
2023-10-23 15:01:54 +0200

    EntornoCliente

--- JavaScript/Teoria/Curso Async y Await/index.js
+++ JavaScript/Teoria/Curso Async y Await/index.js
@@ -14,3 +14,47 @@ const PromiseSuccess = Promise.resolve(1);
 const PromiseRejected = Promise.reject();
 PromiseSuccess.then(console.log);
 PromiseRejected.catch(()=> console.log("This is an error"));
+
+let hello = async () => "hello";
+
+// async function run(){
+//     let value = await hello();
+//     console.log(value);
+//     console.log("after");
+// }
+// run();
+console.log("before");
+
+// REFACTOR PROMESA A ASYNC/AWAIT
+const createImageFromBlob = blob => {
+    let img = new Image();
+    img.src = URL.createObjectURL(blob);
+    return img;
+}
+
+async function imgFetch(){
+    const response = await fetch("https://raw.githubusercontent.com/AlbertoF17/Portfolio/develop/AlbertoFernandezGomez_Portfolio/media/img/icono.png");
+    const blob = await response.blob();
+    const img = createImageFromBlob(blob);
+    img.style.width = "60%";
+    document.body.appendChild(img);
+}
+
+imgFetch().catch(ex => console.error(ex));
+
+const delayValue = (delay, value) => (new Promise((resolve) => {
+    setTimeout(() => resolve(value), delay);
+}))
+
+const p1 = delayValue(1000, 1);
+const p2 = delayValue(2000, 2);
+const p3 = delayValue(3000, 3);
+
+const run = async() => {
+    let values = await Promise.all([p1, p2, p3]);
+    console.log(values);
+    console.log("after2");
+}
+run();
+
+console.log("before2");
